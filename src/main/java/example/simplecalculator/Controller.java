9a02982404d13afad04c32afec460ed99ab5beeb
Shyam-723 <shyam.gupta0723@gmail.com>
2024-08-18 10:04:28 -0700

    Basic number buttons functionalities

--- src/main/java/example/simplecalculator/Controller.java
+++ src/main/java/example/simplecalculator/Controller.java
@@ -1,14 +1,60 @@
 package example.simplecalculator;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private TextField outputTextField;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void buttonOneClick() {
+        outputTextField.appendText("1");
     }
+
+    @FXML
+    void buttonTwoClick() {
+        outputTextField.appendText("2");
+    }
+
+    @FXML
+    void buttonThreeClick() {
+        outputTextField.appendText("3");
+    }
+
+    @FXML
+    void buttonFourClick() {
+        outputTextField.appendText("4");
+    }
+
+    @FXML
+    void buttonFiveClick() {
+        outputTextField.appendText("5");
+    }
+
+    @FXML
+    void buttonSixClick() {
+        outputTextField.appendText("6");
+    }
+
+    @FXML
+    void buttonSevenClick() {
+        outputTextField.appendText("7");
+    }
+
+    @FXML
+    void buttonEightClick() {
+        outputTextField.appendText("8");
+    }
+
+    @FXML
+    void buttonNineClick() {
+        outputTextField.appendText("9");
+    }
+
+    @FXML
+    void buttonZeroClick() {
+        outputTextField.appendText("0");
+    }
+
 }
--- src/main/java/example/simplecalculator/Main.java
+++ src/main/java/example/simplecalculator/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
         Scene scene = new Scene(loader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Simple Calculator");
         stage.setScene(scene);
         stage.show();
     }
